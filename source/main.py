e6ebb91be021b046dc84cfba9f36b92f05a3b92d
pdcnguyen <phamdangcaonguyen@gmail.com>
2023-10-05 16:54:58 +0200

    .

--- source/main.py
+++ source/main.py
@@ -4,7 +4,7 @@ import train
 
 
 def load_and_run_study(study_name, transform=None):
-    train.tune_parameters(250, study_name, transform)
+    # train.tune_parameters(250, study_name, transform)
     study = optuna.load_study(study_name=study_name, storage="sqlite:///results.db")
     print(f"Best params: {study.best_trial.params}")
 
@@ -19,3 +19,5 @@ if __name__ == "__main__":
     study = load_and_run_study(study_name, transform)
 
     train.full_train_and_test(study.best_trial.params, transform)
+
+    # Best params: {'batch_size': 128, 'drop_out': 0.17211139756772692, 'hidden_nodes': 512, 'learning_rate': 0.0012635684485723317, 'optimizer': 'Adam'}

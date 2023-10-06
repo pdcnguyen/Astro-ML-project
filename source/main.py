375b3eec48efb3ba648a4f0269dba31efd524297
pdcnguyen <phamdangcaonguyen@gmail.com>
2023-10-06 17:42:20 +0200

    .

--- source/main.py
+++ source/main.py
@@ -4,7 +4,7 @@ import train
 
 
 def load_and_run_study(study_name, transform=None):
-    # train.tune_parameters(250, study_name, transform)
+    train.tune_parameters(250, study_name, transform)
     study = optuna.load_study(study_name=study_name, storage="sqlite:///results.db")
     print(f"Best params: {study.best_trial.params}")
 
